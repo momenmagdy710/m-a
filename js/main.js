5cd357e46490f778f24f9c25dc14f200c5f7d232
momenmagdy710 <earphonex@gmail.com>
2023-07-02 21:43:43 +0300

    الجمدان كله

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@
         dots: false,
         loop: true,
         nav: true,
-      
+      rtl: true,
         navText : [
             '<i class="bi bi-chevron-left"></i>',
             '<i class="bi bi-chevron-right"></i>'
